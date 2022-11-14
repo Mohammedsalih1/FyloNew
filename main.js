a2794d29b7859020ddba082d08b2e45c33be2908
Mohammedsalih1 <97742110+Mohammedsalih1@users.noreply.github.com>
2022-11-14 21:07:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// // console.log(window.scrollX === window.pageXOffset);
+// let btn = document.querySelector(".up");
+
+// window.onscroll = function () {
+//     if (window.scrollY >= 600) {
+//         btn.style.display = "block";
+//     } else {
+//         btn.style.display = "none";
+//     }
+// };
+
+// btn.onclick = function () {
+//     window.scrollTo({
+//         left: 0,
+//         top: 0,
+//         behavior: "smooth",
+//     });
+// };  
+// let bg = document.querySelectorAll(".section2");
+// let me = document.querySelectorAll("section");
+
+// setInterval(function () {
+    // bg.style.background = "red";
+    // console.log("hi")
+    // me.style.background= "black";
+// }, 2000);
+// console.log(me);
+
+
+// let div = document.querySelector(".cont");
+
+// function count() {
+    // div.innerHTML += 1;
+    // if (div.innerHTML <= "0") {
+        // clearInterval(counter);
+    // }
+// };
+// let counter = setInterval(count, 1000);
